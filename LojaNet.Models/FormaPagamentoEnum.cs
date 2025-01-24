7b4e36dd013bb6d84dd88b46ccb2eff3b9dfe527
N. Menezes <116456129+NathanMenezess@users.noreply.github.com>
2025-01-23 21:59:07 -0300

    Construindo o modelo de dados

---
+++ LojaNet.Models/FormaPagamentoEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LojaNet.Models
+{
+    public enum FormaPagamentoEnum
+    {
+        Dinheiro = 1,
+        Pix = 2,
+        Cartao = 3,
+        Boleto = 4
+
+    }
+}
