923c88f9fbdea95f17f721936be119e860a1ff50
Nathan <menezesnathan61@gmail.com>
2025-01-24 23:38:16 -0300

    Business Rule Layer

---
+++ LojaNet.BLL/ClienteBLL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using LojaNet.Models;
+using LojaNet.DAL;
+
+namespace LojaNet.BLL
+{
+    public class ClienteBLL : IClienteDados
+    {
+        public void Alterar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Incluir(Cliente cliente)
+        {
+            if (string.IsNullOrEmpty(cliente.Nome))
+            {
+                throw new Exception("O nome deve ser informado!");
+            }
+            if(string.IsNullOrEmpty(cliente.Id))
+            {
+                //Gera ID
+                cliente.Id = Guid.NewGuid().ToString();
+            }
+            var dal = new ClienteDAL();
+            dal.Incluir(cliente);
+
+
+        }
+
+        public Cliente ObterPorEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorId(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
