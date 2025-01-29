d5d82350b3f36dc6aa35faf0ad2ed9475d126ba7
Nathan <menezesnathan61@gmail.com>
2025-01-28 22:14:56 -0300

    Customer Onboarding Form

--- LojaNet.UI.Web/Controllers/ClienteController.cs
+++ LojaNet.UI.Web/Controllers/ClienteController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LojaNet.Models;
 
 namespace LojaNet.UI.Web.Controllers
 {
     public class ClienteController : Controller
     {
+        public ActionResult Incluir()
+        {
+            var cli = new Cliente();
+            return View(cli);
+        }
         // GET: Cliente
         public ActionResult Index()
         {
