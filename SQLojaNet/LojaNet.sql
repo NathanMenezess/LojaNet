ca0ae5a1b7b70d7c87310e7396f84dd0d1ecab6e
Nathan <menezesnathan61@gmail.com>
2025-01-30 19:36:58 -0300

    Creating the Database and Stored Procedures

---
+++ SQLojaNet/LojaNet.sql
@@ -0,0 +1,26 @@
+--CREATE DATABASE LojaNetDb
+--GO
+
+--USE LojaNetDb
+--GO
+
+--CREATE TABLE Cliente(
+--	Id varchar(50) PRIMARY KEY,
+--	Nome varchar(100),
+--	Email varchar(100),
+--	Telefone varchar(100)
+--)
+--GO
+
+CREATE PROCEDURE ClienteIncluir(
+@Id varchar(50),
+@Nome varchar(100),
+@Email varchar(100),
+@Telefone varchar(100)
+)
+as 
+
+INSERT INTO Cliente(Id, Nome, Email, Telefone)
+	VALUES(@Id, @Nome, @Email, @Telefone)
+
+	
