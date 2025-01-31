0a0af7e546e545866c300c0b1f1f92763f63a321
Nathan <menezesnathan61@gmail.com>
2025-01-30 21:19:52 -0300

    Data Inclusion

---
+++ LojaNet.DAL/DbHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+
+namespace LojaNet.DAL
+{
+    public static class DbHelper
+    {
+        private static string conexao { 
+            get
+            {
+                return @"Data Source=DESKTOP-JT6KL2F\SQLEXPRESS; 
+                        Initial Catalog=LojaNetDb; 
+                        Integrated Security=true";
+
+            }
+
+
+        }
+        public static int ExecuteNonQuery(string storedProcedure, params object[] parametros )
+        {
+            int retorno = 0; 
+            using (var cn = new SqlConnection(conexao))
+            {
+                using (var cmd = new SqlCommand(storedProcedure, cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    if(parametros.Length > 0 )
+                    {
+                        for(int i = 0; i < parametros.Length; i +=2)
+                        {
+                            cmd.Parameters.AddWithValue(parametros[i].ToString(), parametros[i + 1]);
+                        }
+                    }
+                    cn.Open();
+                    retorno = cmd.ExecuteNonQuery();
+                    cn.Close();
+                } 
+            }
+            return retorno;
+        }
+    }
+}
